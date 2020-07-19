74dfc43fb8c636a9e8bfbe0e92a7c7bb65856e6f
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-07-19 19:43:27 +0900

    test Commit 2020/07/18

---
+++ BASIC/Study1.py
@@ -0,0 +1,13 @@
+def Solve() -> None:
+    languages = ['python', 'perl', 'c']
+
+    for lang in languages:
+        if lang in ['python', 'perl']:
+            print("%6s need interpretor" % lang)
+        elif lang in ['c', 'java']:
+            print("%6s need compiler %6s"%(lang, lang))
+        else:
+            print("should not reach here")
+
+
+Solve()
