15c128bfa9e50c33dbac258717c622c4b04086be
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-07-26 21:51:40 +0900

    Coding test Practice

---
+++ SW_Expert_Academy/1859.py
@@ -0,0 +1,16 @@
+#백만장자 프로젝트
+
+#우선순위 큐 최댓값으로 구현
+class PriorityQueue:
+    def __init__(self):
+        que = []
+
+    def push(self,value):
+        self.que.append(value)
+        if(self.que[value//2] )
+
+
+T = int(input())
+
+for test_case in range (1 , T+1):
+    N = int(input())
