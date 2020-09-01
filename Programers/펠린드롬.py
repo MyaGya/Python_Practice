0825b48927256565083978488bca0585d53444e2
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-09-01 17:03:53 +0900

    Coding Test practice

--- Programers/펠린드롬.py
+++ Programers/펠린드롬.py
@@ -16,3 +16,19 @@ def solution(s):
 
 
 print(solution('abba'))
+
+s = "ABCDEFGA"
+print(id(s))
+print(id(s[0]))
+print(id(s[len(s)-1]))
+print(id(s[2]))
+if s[0] is s[len(s)-1]:
+    print(True)
+tmp = 'A'
+print(id(tmp))
+
+test1 = (1,2)
+test2 = (1,2)
+print(id(test1))
+print(id(test2))
+
