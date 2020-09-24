248040aa1e90a4213805656505621315623f25c6
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-09-24 15:36:45 +0900

    숫자 블록

--- Programers/숫자 블록.py
+++ Programers/숫자 블록.py
@@ -3,12 +3,15 @@
 import math
 def solution(begin, end):
     ret = []
+    set_block = 10**7 # 블록이 깔려있는 위치
     for i in range(begin,end+1):
         if i < 2:
             ret.append(0)
             continue
-        for j in range(2,int(math.sqrt(i))+1):
+        for j in range(2,int(math.sqrt(i))+1): # 1일때에는 해당 안되기때문에 2부터 계산한다
             if i % j == 0:
+                if i//j > set_block:
+                    continue
                 ret.append(i//j)
                 break
         else:
