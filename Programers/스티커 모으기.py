55f48172ea37eac0e90b672db120e5c642803538
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-09-25 23:58:32 +0900

    스티커 모으기

--- Programers/스티커 모으기.py
+++ Programers/스티커 모으기.py
@@ -1,9 +1,11 @@
 def solution(sticker):
+    if len(sticker) == 1: # 예외처리
+        return sticker[0]
     dp_start0 = [0 for _ in range(len(sticker))]
     dp_start1 = [0 for _ in range(len(sticker))]
 
     dp_start0[0] = sticker[0]
-    dp_start0[1] = sticker[1]
+    dp_start0[1] = max(sticker[0],sticker[1])
     dp_start1[0] = 0
     dp_start1[1] = sticker[1]
     for i in range(2, len(sticker)):
