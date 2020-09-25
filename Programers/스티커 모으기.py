87c60a06a79576c0531a72b9f1e5d39eb94b2220
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-09-25 23:22:01 +0900

    스티커 모으기

--- Programers/스티커 모으기.py
+++ Programers/스티커 모으기.py
@@ -1,10 +1,14 @@
 def solution(sticker):
-    dp_start0 = []
-    dp_start1 = []
+    dp_start0 = [0 for _ in range(len(sticker))]
+    dp_start1 = [0 for _ in range(len(sticker))]
 
     dp_start0[0] = sticker[0]
     dp_start0[1] = sticker[1]
     dp_start1[0] = 0
     dp_start1[1] = sticker[1]
-    for i in range(2, len(stickers)):
-        dp_start0[i] = dp_
+    for i in range(2, len(sticker)):
+        dp_start0[i] = max(dp_start0[i-2] + sticker[i],dp_start0[i-1])
+        dp_start1[i] = max(dp_start1[i-2] + sticker[i],dp_start1[i-1])
+    return max(dp_start0[-2],dp_start1[-1])
+
+print(solution([14, 6, 5, 11, 3, 9, 2, 10]))
