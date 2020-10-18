0bf212265c25232076b7442d0e0a645aa81d00f7
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-10-18 22:34:28 +0900

    여행경로

--- Programers/여행경로.py
+++ Programers/여행경로.py
@@ -1,17 +1,45 @@
 from collections import defaultdict
-import heapq
 
 
-def solution(tickets: [str]):
-    ticket = dict
+def dfs(depart, route, route_count, ticket_count, history):
+    '''
+    depart : 출발지
+    destination : 도착지
+    route : 가능한 경로
+    route_count : 가능한 경로의 갯수
+    ticket_count : 남은 티켓 갯수
+    history : 지나온 경로
+    '''
 
-    tickets.sort(key=lambda x: x[1])
-    for Lst in tickets:
-        ticket[Lst[0]].append(Lst[1])
+    history.append(depart)  # 지나온 경로 추가
 
-    queue = deque("ICN")
+    if not ticket_count:  # end cond
+        return True
 
-    while queue:
-        data = queue.popleft()
+    for destination in route[depart]:
+        if route_count[tuple((depart, destination))]:
+            route_count[tuple((depart, destination))] -= 1
+            if dfs(destination, route, route_count, ticket_count-1, history):
+                return True
+            route_count[tuple((depart, destination))] += 1
 
-print(solution([['ICN', 'A'], ['ICN', 'A'], ['A', 'ICN']]))
+    history.pop()  # 지나온 경로 해제
+    return False
+
+def solution(tickets):
+    # 다시 돌아왔다 가는 경로도 생각해야한다. 즉, 특정 위치에서 다른 위치로 몇 번이나 갈 수 있는지를 체크해야한다
+    route_count = defaultdict(int)
+    route = defaultdict(set)
+    ticket_count = len(tickets)
+
+    for ticket in tickets:
+        route_count[tuple(ticket)] += 1
+        route[ticket[0]].add(ticket[1])
+
+    for key in route.keys():                    # 사전순으로 탐색
+        route[key] = sorted(list(route[key]))
+    ret = []
+    dfs("ICN", route, route_count, ticket_count,ret)
+    return ret
+
+print(solution([['ICN', 'A'], ['A', 'C'], ['A', 'D'], ['D', 'B'], ['B', 'A']]))
