76440f94fc477d758ef855780a24fce596dd6968
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-11-05 23:52:53 +0900

    K번째수 2016년

--- Programers/K번째수.py
+++
@@ -1,6 +0,0 @@
-def solution(array, commands):
-    answer = []
-    for s in commands:
-        answer.append(sorted(array[s[0]-1:s[1]])[s[2]-1])
-    return answer
-print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
