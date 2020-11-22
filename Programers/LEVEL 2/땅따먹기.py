d8402480a19016495a54db9a1430863ca8c76d25
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-11-22 19:07:23 +0900

    땅따먹기

--- Programers/LEVEL 2/땅따먹기.py
+++ Programers/LEVEL 2/땅따먹기.py
@@ -1,3 +1,4 @@
+'''
 def solution(land):
     # init
     pick = [[0 for _ in range(4)] for _ in range(len(land))]
@@ -11,4 +12,12 @@ def solution(land):
 
     return max(pick[i])
 
+
+'''
+
+def solution(land):
+    for i in range(1, len(land)):
+        for j in range(4):
+            land[i][j] += max(land[i-1][(j+1)%4],land[i-1][(j+2)%4],land[i-1][(j+3)%4])
+    return max(land[i])
 print(solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]]))
