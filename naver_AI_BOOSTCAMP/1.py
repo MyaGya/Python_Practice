fdcf58c098d3d7a867a8e78e399209eebf1c84e4
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-01-05 18:29:47 +0900

    commit

--- naver_AI_BOOSTCAMP/1.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-def solution(s):
-    ans = 0
-    for i in Counter(s).values():
-        if i %2 == 1:
-            ans += 1
-    return ans
-
-print(solution("a"))
