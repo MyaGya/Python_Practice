592a52583045ada4a6630d3e183bb91322818628
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-04-25 22:08:32 +0900

    H-index

---
+++ Programers_New_Start/level2/H-index.py
@@ -0,0 +1,6 @@
+def solution(citations):
+    citations.sort()
+    for i in range(len(citations)):
+        if citations[i] >= len(citations) - i:  # 인용된 횃수가 >= 인용된 논문의 갯수
+            return len(citations) - i
+    return 0
